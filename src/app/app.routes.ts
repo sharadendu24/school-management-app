ab79a03b8dfb684f76cf16e268f321d8c5bb58ab
sharadendu24 <tripathi0sharadendu@gmail.com>
2025-04-20 22:24:51 +0530

    added front page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth-components/login/login.component';
 import { RegisterComponent } from './auth-components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
+import { FrontPageComponent } from './components/front-page/front-page.component';
 
 export const routes: Routes = [
+    {path: '', component:FrontPageComponent},
     {path: 'login', component:LoginComponent},
     {path: 'register', component:RegisterComponent},
     {path: 'home', component:HomeComponent}
